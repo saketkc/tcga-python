6c70f4f4149b353e110befd15b5e9388289183a6
Saket Choudhary <saketkc@gmail.com>
2014-04-22 04:32:12 +0530

    temp commit

--- TCGA/TCGAwww.py
+++ TCGA/TCGAwww.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 import requests
-__tcga_url__ = "http://tcga-data.nci.nih.gov/tcgadccws/Get"
+import unittest
+__tcga_url__ = 'http://tcga-data.nci.nih.gov/tcgadccws/Get'
 class TCGADownloader():
 
-    def __init__( self, url=__tcga_url__, retrieval_format="XML" ):
+    def __init__(self, url=__tcga_url__):
         self.url = url
-        assert retrieval_format in ["XML", "HTML"]
-        self.retrieval_format = retrieval_format
 
-    def  make_query( query_term=None):
-        params = {}
-        self.request = requests.get(self.url, )
+    def make_query(retrieval_format, params):
+        assert retrieval_format in ['XML', 'HTML']
+        url = self.url + retrieval_format
+        request = requests.get(url, params=params)
+        response = request.text
+        return response
+
+    def get_diseases():
+        return make_query('XML', {'query': Diseases})
 
 
+class TCGADownloaderTestFunctions(unittests):
+    def setUP(self):
+        self.setup = TCGADownloader()
 
-
+    def test_make_query(self):
+        query = make_query()
